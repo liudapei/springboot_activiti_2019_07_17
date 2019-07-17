14a6c162e357f5156ba2b6a5c2d3cbe49fe6b234
liuyupei <793211899@qq.com>
2019-07-17 09:35:53 +0800

    Demo

--- springboot_activiti_demo/src/main/java/com/demo/springboot_activiti_demo/SpringbootActivitiDemoApplication.java
+++ springboot_activiti_demo/src/main/java/com/demo/springboot_activiti_demo/SpringbootActivitiDemoApplication.java
@@ -1,9 +1,10 @@
 package com.demo.springboot_activiti_demo;
 
+import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SpringbootActivitiDemoApplication {
 
     public static void main(String[] args) {
---
+++ springboot_activiti_demo/src/main/java/com/demo/springboot_activiti_demo/controller/ActivitiController.java
@@ -0,0 +1,61 @@
+package com.demo.springboot_activiti_demo.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.activiti.editor.constants.ModelDataJsonConstants;
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.repository.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/activiti")
+public class ActivitiController {
+    //1244 测试
+    @RequestMapping("/aa")
+    public String aa(){
+        return "测试数据";
+    }
+
+    /** 1244
+     * 创建模型
+     */
+    @RequestMapping("/create")
+    public void create(HttpServletRequest request, HttpServletResponse response) {
+
+        try {
+            ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+
+            RepositoryService repositoryService = processEngine.getRepositoryService();
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            ObjectNode editorNode = objectMapper.createObjectNode();
+            editorNode.put("id", "canvas");
+            editorNode.put("resourceId", "canvas");
+            ObjectNode stencilSetNode = objectMapper.createObjectNode();
+            stencilSetNode.put("namespace", "http://b3mn.org/stencilset/bpmn2.0#");
+            editorNode.put("stencilset", stencilSetNode);
+            Model modelData = repositoryService.newModel();
+
+            ObjectNode modelObjectNode = objectMapper.createObjectNode();
+            modelObjectNode.put(ModelDataJsonConstants.MODEL_NAME, "hello1111");
+            modelObjectNode.put(ModelDataJsonConstants.MODEL_REVISION, 1);
+            String description = "hello1111";
+            modelObjectNode.put(ModelDataJsonConstants.MODEL_DESCRIPTION, description);
+            modelData.setMetaInfo(modelObjectNode.toString());
+            modelData.setName("hello1111");
+            modelData.setKey("12313123");
+            //保存模型
+            repositoryService.saveModel(modelData);
+            repositoryService.addModelEditorSource(modelData.getId(), editorNode.toString().getBytes("utf-8"));
+            response.sendRedirect(request.getContextPath() + "/modeler.html?modelId=" + modelData.getId());
+        } catch (Exception e) {
+            System.out.println("创建模型失败：");
+        }
+    }
+}
